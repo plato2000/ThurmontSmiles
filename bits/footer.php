6042b7c96d8663c85603c03fd4da1060dada11cc
Parth Oza <plato2000@rajeevs-imac.home>
2013-12-23 21:43:39 -0500

    Added stuff to footer

--- bits/footer.php
+++ bits/footer.php
@@ -1,3 +1,4 @@
 <div class="footer">
-  <p>&copy; Thurmont Smiles 2013</p>
+	<address>100 S. Center St. Thurmont, MD 21788</address><br />
+  <p>&copy; Thurmont Smiles 2013 | <a href="privacy.php">Patient Privacy</a> | <a href="sitemap.php">Site Map</a> | <a href="disclaim.php">Disclaimer</a></p>
 </div>
